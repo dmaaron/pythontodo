05e269cebffd9079553c7150865b9896f6fb3147
David Aaron <davidmatthewaaron@gmail.com>
2014-07-12 17:07:24 -0400

    adds task creation to views

--- todolist/views.py
+++ todolist/views.py
@@ -5,4 +5,7 @@ def tasks(request):
     template_name='tasks.html'
     tasks=Task.objects.all()
     context={'tasks':tasks}
+    if request.method == 'POST':
+        task_description = request.POST.get('task')
+        Task.objects.create(description=task_description)        
     return render(request, template_name, context)
