8f67d4af900de35f8b9735df1725d28ccfc45704
David Aaron <davidmatthewaaron@gmail.com>
2014-07-12 16:49:07 -0400

    adds tasks view, and template. Modifies model to display Task description text

--- todolist/models.py
+++ todolist/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     description=models.CharField(max_length=140)
+
+    def __unicode__(self):
+        return self.description
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from todolist.models import Task
 # Create your views here.
+def tasks(request):
+    template_name='tasks.html'
+    tasks=Task.objects.all()
+    context={'tasks':tasks}
+    return render(request, template_name, context)
