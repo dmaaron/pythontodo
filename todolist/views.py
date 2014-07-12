9044ba190982581fb08cd9b8ef1bf984740dcb03
David Aaron <davidmatthewaaron@gmail.com>
2014-07-12 17:27:07 -0400

    adds twilio text functionality

--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from todolist.models import Task
+from twilio.rest import TwilioRestClient
+from django.conf import settings
+
 # Create your views here.
 def tasks(request):
     template_name='tasks.html'
@@ -7,5 +10,13 @@ def tasks(request):
     context={'tasks':tasks}
     if request.method == 'POST':
         task_description = request.POST.get('task')
-        Task.objects.create(description=task_description)        
+        Task.objects.create(description=task_description)
+        with open('{}/todolist/digits.txt'.format(settings.BASE_DIR), 'r') as phone_number_file:
+            phone_number = phone_number_file.readlines()[0]
+        account_SID='AC29a92553fae2bfdf83a2ec5e09bfec87'
+        auth_token='a437cf3cf441bd04cfbb31ef858f03bf'
+        client=TwilioRestClient(account_SID,auth_token)
+        message = client.messages.create(body="Congrats, you added the following task: {}".format(task_description),
+            to=phone_number,
+            from_="+16467627640" )       
     return render(request, template_name, context)
